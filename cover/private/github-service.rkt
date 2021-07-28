88b9b2251183e0e4a6cf41b192cb6a4ce91dbf8c
Suzanne Soy <jsmaniac.github@suzanne.soy>
2021-07-28 17:10:40 +0100

    Trying to add github service

---
+++ cover/private/github-service.rkt
@@ -0,0 +1,21 @@
+#lang racket/base
+(provide github-service@ github-env?)
+
+(require "ci-service.rkt" racket/unit racket/list racket/string)
+
+(define (github-ci?) (and (getenv "GITHUB_REPOSITORY") (getenv "CODECOV_TOKEN")))
+
+(define-unit github-service@
+  (import)
+  (export ci-service^)
+
+  (define (query)
+    (define repo-slug (getenv "GITHUB_REPOSITORY"))
+    (list (cons 'service "custom")
+          (cons 'token (getenv "CODECOV_TOKEN"))
+          ;; TODO: this won't work for tags
+          (cons 'branch (substring (getenv "GITHUB_REF") (string-length "refs/heads/")))
+          (cons 'job (getenv "GITHUB_JOB"))
+          (cons 'slug (getenv "GITHUB_REPOSITORY"))
+          (cons 'build (getenv "GITHUB_RUN_ID"))
+          (cons 'commit (getenv "GITHUB_SHA")))))
